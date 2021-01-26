f2006eb525ca6f1c3d58755ddbbde5b619cd0186
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-26 14:34:06 +0100

    Updates

---
+++ precache-manifest.b24a9c5c0309f3ede703823d3c12e288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4fb615ecedcfef1caea30e44359251",
+    "url": "/randomquotemachinebetter/index.html"
+  },
+  {
+    "revision": "e8dbcd0648b7d0db0d16",
+    "url": "/randomquotemachinebetter/static/css/main.ea1e7483.chunk.css"
+  },
+  {
+    "revision": "4a5ed4044a73579621fe",
+    "url": "/randomquotemachinebetter/static/js/2.704df43d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/randomquotemachinebetter/static/js/2.704df43d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8dbcd0648b7d0db0d16",
+    "url": "/randomquotemachinebetter/static/js/main.9f219ae9.chunk.js"
+  },
+  {
+    "revision": "a362deea95629c731ed4",
+    "url": "/randomquotemachinebetter/static/js/runtime-main.adfb5347.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/randomquotemachinebetter/precache-manifest.b24a9c5c0309f3ede703823d3c12e288.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/randomquotemachinebetter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
